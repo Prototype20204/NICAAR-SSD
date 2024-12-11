9858833d121572bd72ea5dc79ffa31c562c25472
Mach Deng Mach <freedom.hotel09@gmail.com>
2024-12-11 18:07:33 +0200

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css';
-import { WeatherDataProvider } from '@/utils/WeatherDataReducer';
+import '@/globals.css';
+import { WeatherDataProvider } from '@/WeatherDataReducer';
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
